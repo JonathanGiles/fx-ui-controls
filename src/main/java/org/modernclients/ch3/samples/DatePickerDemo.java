7b14b9bcbebf3d6dfdcd418c89ff3b99bf01e669
Fouad Almalki <me@fouad.io>
2018-07-08 04:37:51 +0300

    add disable text editing to DatePicker sample

--- src/main/java/org/modernclients/ch3/samples/DatePickerDemo.java
+++ src/main/java/org/modernclients/ch3/samples/DatePickerDemo.java
@@ -8,16 +8,21 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.util.Callback;
+import javafx.util.StringConverter;
 import org.modernclients.ch3.Sample;
 
 import java.time.LocalDate;
 import java.time.chrono.HijrahChronology;
 import java.time.chrono.IsoChronology;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class DatePickerDemo implements Sample {
-    
+ 
+	private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
     private DatePicker datePicker;
     private boolean disableFutureDaysSelection;
     
@@ -26,17 +31,41 @@ public class DatePickerDemo implements Sample {
         datePicker = new DatePicker();
 	    datePicker.setChronology(IsoChronology.INSTANCE);
 	
+	    Predicate<LocalDate> dateValidator = localDate -> localDate.isAfter(LocalDate.now());
+	
 	    Callback<DatePicker, DateCell> dayCellFactory = dp -> new DateCell() {
 		    @Override
-		    public void updateItem(LocalDate item, boolean empty) {
-			    super.updateItem(item, empty);
+		    public void updateItem(LocalDate date, boolean empty) {
+			    super.updateItem(date, empty);
 			
-			    if(disableFutureDaysSelection && item.isAfter(LocalDate.now())) {
+			    if(disableFutureDaysSelection && dateValidator.test(date)) {
 				    Platform.runLater(() -> setDisable(true));
 			    }
 		    }
 	    };
 	    datePicker.setDayCellFactory(dayCellFactory);
+	
+	    StringConverter<LocalDate> converter = new StringConverter<LocalDate>() {
+		    @Override
+		    public String toString(LocalDate date) {
+			    if(date != null) return dateFormatter.format(date);
+			    else return "";
+		    }
+		
+		    @Override
+		    public LocalDate fromString(String string) {
+			    if(string != null && !string.trim().isEmpty()) {
+				    LocalDate date = LocalDate.parse(string, dateFormatter);
+				
+				    if(disableFutureDaysSelection && dateValidator.test(date)) {
+					    return datePicker.getValue(); // the old value
+				    }
+				    else return date;
+			    }
+			    else return null;
+		    }
+	    };
+	    datePicker.setConverter(converter);
         
         container.getChildren().addAll(datePicker);
     }
@@ -59,12 +88,16 @@ public class DatePickerDemo implements Sample {
 		    }
 		    else datePicker.setChronology(IsoChronology.INSTANCE);
 	    });
+	
+	    CheckBox cbDisableTextEditing = new CheckBox("Disable text editing");
+	    cbDisableTextEditing.selectedProperty().addListener((observable, oldValue, newValue) ->
+			                                                      datePicker.getEditor().setEditable(!newValue));
 	    
 	    CheckBox cbDisableFutureDaysSelection = new CheckBox("Disable future days selection");
 	    cbDisableFutureDaysSelection.selectedProperty().addListener((observable, oldValue, newValue) ->
 		                                                                disableFutureDaysSelection = newValue);
     	
-        VBox vBox = new VBox(cbShowPromptText, cbUseHijrahCalendar, cbDisableFutureDaysSelection);
+        VBox vBox = new VBox(cbShowPromptText, cbUseHijrahCalendar, cbDisableTextEditing, cbDisableFutureDaysSelection);
 	    vBox.setSpacing(5.0);
         return Optional.of(vBox);
     }
