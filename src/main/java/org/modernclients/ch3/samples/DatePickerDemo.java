40af75f5e9216a4fbd4d9cbeb4fe3a8ddfb0c118
Fouad Almalki <me@fouad.io>
2018-07-08 06:20:29 +0300

    convert tabs to spaces

--- src/main/java/org/modernclients/ch3/samples/DatePickerDemo.java
+++ src/main/java/org/modernclients/ch3/samples/DatePickerDemo.java
@@ -1,6 +1,5 @@
 package org.modernclients.ch3.samples;
 
-import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -26,116 +25,116 @@ import java.util.function.Predicate;
  */
 public class DatePickerDemo implements Sample {
  
-	private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    
     private DatePicker datePicker;
     private boolean disableRangeSelection;
     private Predicate<LocalDate> dateValidator = localDate -> {
-    	if(disableRangeSelection) {
-    		return localDate != null && !localDate.isAfter(LocalDate.now());
-	    }
-	    else return localDate != null;
+        if(disableRangeSelection) {
+            return localDate != null && !localDate.isAfter(LocalDate.now());
+        }
+        else return localDate != null;
     };
     
     @Override
     public void buildDemo(Pane container, Consumer<String> console) {
         datePicker = new DatePicker();
-	    datePicker.setChronology(IsoChronology.INSTANCE);
-	    datePicker.setOnAction(e -> {
-		    LocalDate date = datePicker.getValue();
-		    console.accept("Selected date: " + (date != null ? dateFormatter.format(date) : null));
-	    });
-	
-	    Callback<DatePicker, DateCell> dayCellFactory = dp -> new DateCell() {
-		    @Override
-		    public void updateItem(LocalDate date, boolean empty) {
-			    super.updateItem(date, empty);
-			
-			    if(!dateValidator.test(date)) {
-				    setDisable(true);
-			    }
-		    }
-	    };
-	    datePicker.setDayCellFactory(dayCellFactory);
-	
-	    StringConverter<LocalDate> converter = new StringConverter<LocalDate>() {
-		    @Override
-		    public String toString(LocalDate date) {
-			    if(date != null) return dateFormatter.format(date);
-			    else return "";
-		    }
-		
-		    @Override
-		    public LocalDate fromString(String string) {
-			    if(string != null && !string.trim().isEmpty()) {
-				
-				    LocalDate oldValue = datePicker.getValue();
-				    LocalDate newValue;
-			    	
-			    	try {
-					    newValue = LocalDate.parse(string, dateFormatter);
-				    }
-				    catch(DateTimeException e) {
-				    	console.accept("The text \"" + string + "\" does not match the pattern \"dd/MM/yyyy\"");
-				    	return oldValue;
-				    }
-				
-				    if(!dateValidator.test(newValue)) {
-					    console.accept("The date (" + dateFormatter.format(newValue) + ") is not allowed!");
-					    return oldValue;
-				    }
-				    else return newValue;
-			    }
-			    else return null;
-		    }
-	    };
-	    datePicker.setConverter(converter);
+        datePicker.setChronology(IsoChronology.INSTANCE);
+        datePicker.setOnAction(e -> {
+            LocalDate date = datePicker.getValue();
+            console.accept("Selected date: " + (date != null ? dateFormatter.format(date) : null));
+        });
+    
+        Callback<DatePicker, DateCell> dayCellFactory = dp -> new DateCell() {
+            @Override
+            public void updateItem(LocalDate date, boolean empty) {
+                super.updateItem(date, empty);
+            
+                if(!dateValidator.test(date)) {
+                    setDisable(true);
+                }
+            }
+        };
+        datePicker.setDayCellFactory(dayCellFactory);
+    
+        StringConverter<LocalDate> converter = new StringConverter<LocalDate>() {
+            @Override
+            public String toString(LocalDate date) {
+                if(date != null) return dateFormatter.format(date);
+                else return "";
+            }
+        
+            @Override
+            public LocalDate fromString(String string) {
+                if(string != null && !string.trim().isEmpty()) {
+                
+                    LocalDate oldValue = datePicker.getValue();
+                    LocalDate newValue;
+                    
+                    try {
+                        newValue = LocalDate.parse(string, dateFormatter);
+                    }
+                    catch(DateTimeException e) {
+                        console.accept("The text \"" + string + "\" does not match the pattern \"dd/MM/yyyy\"");
+                        return oldValue;
+                    }
+                
+                    if(!dateValidator.test(newValue)) {
+                        console.accept("The date (" + dateFormatter.format(newValue) + ") is not allowed!");
+                        return oldValue;
+                    }
+                    else return newValue;
+                }
+                else return null;
+            }
+        };
+        datePicker.setConverter(converter);
         
         container.getChildren().addAll(datePicker);
     }
     
     @Override
     public Optional<Node> buildControlPanel() {
-	    CheckBox cbShowPromptText = new CheckBox("Show prompt text (dd/MM/yyyy)");
-	    cbShowPromptText.selectedProperty().addListener((observable, oldValue, newValue) -> {
-		    if(newValue) { // selected
-			    datePicker.setPromptText("dd/MM/yyyy");
-		    }
-		    else datePicker.setPromptText("");
-	    });
-	    cbShowPromptText.setSelected(true);
-	
-	    CheckBox cbShowWeekNumbers = new CheckBox("Show week numbers");
-	    cbShowWeekNumbers.selectedProperty().addListener((observable, oldValue, newValue) ->
-			                                                     datePicker.setShowWeekNumbers(newValue));
-    	
-	    CheckBox cbShowHijrahCalendar = new CheckBox("Show hijrah calendar");
-	    cbShowHijrahCalendar.selectedProperty().addListener((observable, oldValue, newValue) -> {
-	    	if(newValue) { // selected
-			    datePicker.setChronology(HijrahChronology.INSTANCE);
-		    }
-		    else datePicker.setChronology(IsoChronology.INSTANCE);
-	    });
-	
-	    CheckBox cbDisableTextEditing = new CheckBox("Disable text editing");
-	    cbDisableTextEditing.selectedProperty().addListener((observable, oldValue, newValue) ->
-			                                                      datePicker.getEditor().setEditable(!newValue));
-	    
-	    CheckBox cbDisableRangeSelection = new CheckBox("Disable range selection (e.g. future dates)");
-	    cbDisableRangeSelection.selectedProperty().addListener((observable, oldValue, newValue) -> {
-			disableRangeSelection = newValue;
-		
-		    if(!dateValidator.test(datePicker.getValue())) {
-			    datePicker.setValue(null);
-			}
-	    });
-	
-	    Button btnToday = new Button("Today");
-	    btnToday.setOnAction(event -> datePicker.setValue(LocalDate.now()));
-    	
+        CheckBox cbShowPromptText = new CheckBox("Show prompt text (dd/MM/yyyy)");
+        cbShowPromptText.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue) { // selected
+                datePicker.setPromptText("dd/MM/yyyy");
+            }
+            else datePicker.setPromptText("");
+        });
+        cbShowPromptText.setSelected(true);
+    
+        CheckBox cbShowWeekNumbers = new CheckBox("Show week numbers");
+        cbShowWeekNumbers.selectedProperty().addListener((observable, oldValue, newValue) ->
+                                                                 datePicker.setShowWeekNumbers(newValue));
+        
+        CheckBox cbShowHijrahCalendar = new CheckBox("Show hijrah calendar");
+        cbShowHijrahCalendar.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue) { // selected
+                datePicker.setChronology(HijrahChronology.INSTANCE);
+            }
+            else datePicker.setChronology(IsoChronology.INSTANCE);
+        });
+    
+        CheckBox cbDisableTextEditing = new CheckBox("Disable text editing");
+        cbDisableTextEditing.selectedProperty().addListener((observable, oldValue, newValue) ->
+                                                                  datePicker.getEditor().setEditable(!newValue));
+        
+        CheckBox cbDisableRangeSelection = new CheckBox("Disable range selection (e.g. future dates)");
+        cbDisableRangeSelection.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            disableRangeSelection = newValue;
+        
+            if(!dateValidator.test(datePicker.getValue())) {
+                datePicker.setValue(null);
+            }
+        });
+    
+        Button btnToday = new Button("Today");
+        btnToday.setOnAction(event -> datePicker.setValue(LocalDate.now()));
+        
         VBox vBox = new VBox(cbShowPromptText, cbShowWeekNumbers, cbShowHijrahCalendar,
                              cbDisableTextEditing, cbDisableRangeSelection, btnToday);
-	    vBox.setSpacing(5.0);
+        vBox.setSpacing(5.0);
         return Optional.of(vBox);
     }
 }
