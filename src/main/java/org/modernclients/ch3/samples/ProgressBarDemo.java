a9bee1099b762ff1ac890bc77866818e0370e14e
Jonathan Giles <jonathan@jonathangiles.net>
2018-07-19 15:12:25 +1200

    Misc improvements to the progress indicator / progress bar demos

--- src/main/java/org/modernclients/ch3/samples/ProgressBarDemo.java
+++ src/main/java/org/modernclients/ch3/samples/ProgressBarDemo.java
@@ -1,21 +1,48 @@
 package org.modernclients.ch3.samples;
 
-import javafx.scene.control.ProgressBar;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import org.modernclients.ch3.Sample;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 
 /**
  * @author Jonathan Giles <jonathan@jonathangiles.net>
  */
 public class ProgressBarDemo implements Sample {
+    // No initial value of the ProgressBar is set, so will have an indeterminate indicator
+    final ProgressBar progressBar = new ProgressBar();
 
     @Override
     public void buildDemo(Pane container, Consumer<String> console) {
-        ProgressBar progressBar = new ProgressBar();
-        progressBar.setProgress(0.25f);
+        container.getChildren().addAll(progressBar);
+    }
 
-        container.getChildren().add(progressBar);
+    @Override
+    public Optional<Node> buildControlPanel() {
+        CheckBox indeterminateCheckBox = new CheckBox();
+        indeterminateCheckBox.setSelected(true);
+
+        Slider progressSlider = new Slider(0.0f, 1.0f, 0.5f);
+        progressSlider.disableProperty().bind(indeterminateCheckBox.selectedProperty());
+
+        indeterminateCheckBox.selectedProperty().addListener(o -> {
+            if (indeterminateCheckBox.isSelected()) {
+                progressBar.progressProperty().unbind();
+                progressBar.setProgress(-1);
+            } else {
+                progressBar.progressProperty().bind(progressSlider.valueProperty());
+            }
+        });
+
+        VBox vBox = new VBox(10,
+                new Label("Indeterminate:"),
+                indeterminateCheckBox,
+                new Label("Progress:"),
+                progressSlider);
+        return Optional.of(vBox);
     }
 }
--- src/main/java/org/modernclients/ch3/samples/ProgressIndicatorDemo.java
+++ src/main/java/org/modernclients/ch3/samples/ProgressIndicatorDemo.java
@@ -1,13 +1,13 @@
 package org.modernclients.ch3.samples;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ProgressIndicator;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import org.modernclients.ch3.Sample;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 
 /**
@@ -15,36 +15,36 @@ import java.util.function.Consumer;
  * @author Frank Delporte <frank@webtechie.be>
  */
 public class ProgressIndicatorDemo implements Sample {
+    // No initial value of the ProgressIndicator is set, so will have an indeterminate indicator
+    final ProgressIndicator progressIndicator = new ProgressIndicator();
+
     @Override
     public void buildDemo(Pane container, Consumer<String> console) {
-    	// We will align some components vertically
-    	final VBox holder = new VBox();
-    	holder.prefWidthProperty().bind(container.prefWidthProperty());
-    	holder.prefHeightProperty().bind(container.prefHeightProperty());
-    	container.getChildren().add(holder);
-    	
-    	// Construct the ProgressIndicator
-    	// No initial value of the ProgressIndicator is set, so will have an indeterminated value
-        final ProgressIndicator progressIndicator = new ProgressIndicator();
-    	
-    	// Add a ButtonBar instance to demo different functions of the ProgressIndicator
-        final ButtonBar buttonBar = new ButtonBar();
+        container.getChildren().addAll(progressIndicator);
+    }
 
-        // Construct multiple buttons for demo cases
-        final Button setTo0 = new Button("Set to indeterminated");
-        setTo0.setOnAction(event -> progressIndicator.setProgress(-1));
-        
-        final Button setTo50 = new Button("Set to 50%");
-        setTo50.setOnAction(event -> progressIndicator.setProgress(0.5f));
-        
-        final Button setTo75 = new Button("Set to 75%");
-        setTo75.setOnAction(event -> progressIndicator.setProgress(0.75f));
+    @Override
+    public Optional<Node> buildControlPanel() {
+        CheckBox indeterminateCheckBox = new CheckBox();
+        indeterminateCheckBox.setSelected(true);
 
-        // Add buttons to the ButtonBar
-        buttonBar.getButtons().addAll(setTo0, setTo50, setTo75);
-        holder.getChildren().addAll(buttonBar);
-    	
-    	// Add it to the screen
-        holder.getChildren().add(progressIndicator);
+        Slider progressSlider = new Slider(0.0f, 1.0f, 0.5f);
+        progressSlider.disableProperty().bind(indeterminateCheckBox.selectedProperty());
+
+        indeterminateCheckBox.selectedProperty().addListener(o -> {
+            if (indeterminateCheckBox.isSelected()) {
+                progressIndicator.progressProperty().unbind();
+                progressIndicator.setProgress(-1);
+            } else {
+                progressIndicator.progressProperty().bind(progressSlider.valueProperty());
+            }
+        });
+
+        VBox vBox = new VBox(10,
+                new Label("Indeterminate:"),
+                indeterminateCheckBox,
+                new Label("Progress:"),
+                progressSlider);
+        return Optional.of(vBox);
     }
 }
